dac13bcd2a8a26d0fbc808b3f2c51a477faad308
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2024-03-29 22:31:30 -0500

    change ftp server credentials

--- quasar.conf.js
+++ quasar.conf.js
@@ -135,7 +135,7 @@ module.exports = function (/* ctx */) {
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#000000',
         icons: [
           {
             src: 'icons/icon-128x128.png',
